69cafd44dca3d9474d0ebd722e042c839dfbc528
Aoba <hiroaki.aoba@nextremer.com>
2018-10-26 19:32:32 +0900

    Add description comment

--- app.js
+++ app.js
@@ -16,15 +16,22 @@ function handler(req, res) {
     res.end();
   });
 }
-
+// [chat]namespaceを作成する
+// connectionでweb socket接続
 const chat = io.of('/chat').on('connection', function(socket) {
+  // onで[emit_from_client]というイベントを待ち受ける
+  // clientでemitされたdataを受け取る
   socket.on('emit_from_client', function(data) {
+    // 送られてきたroom名に自身のパイプをjoinさせる
     socket.join(data.room);
+    // 自身のパイプに対してdataを送る
     socket.emit('emit_from_server', 'you are in ' + data.room);
+    // 自身のパイプ以外の通信に対してdataを送る
     socket.broadcast.to(data.room).emit('emit_from_server', data.msg);
   });
 });
 
+// [news]namespaceを作成する
 const news = io.of('/news').on('connection', function(socket) {
   let date = new Date(),
       year = date.getFullYear();
@@ -33,5 +40,6 @@ const news = io.of('/news').on('connection', function(socket) {
       hour = date.getHours();
       minute = date.getMinutes();
   date = year + '/' + month + '/' + day + ' ' + hour + ':' + minute
+  // 自身のパイプに対してdataを送る
   socket.emit('emit_from_server', 'today: ' + date );
 });
